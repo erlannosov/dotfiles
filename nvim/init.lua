89b01915041126d2c10dce627a3932236430a7be
Ayananta Paul <nerdspective0@gmail.com>
2026-05-08 21:14:30 +0530

    fixed typo

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 -- This is a very minimal NeoVim config file for them who want 
--- to use NeoVim but with (vanilla) VIM appearance and Neovim features 
+-- to use NeoVim but with VIM appearance and Neovim features 
 
 -- enable line number
 vim.wo.number = true 
